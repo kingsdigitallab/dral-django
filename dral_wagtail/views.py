297c3f2610e9b5ff1f161ce7e7aa5ac88e25dc4e
Ginestra Ferraro <ginestra@users.noreply.github.com>
2018-05-15 18:46:22 +0100

    Adding the calendar view to visualizations

--- dral_wagtail/views.py
+++ dral_wagtail/views.py
@@ -103,6 +103,50 @@ def visualisation_svg_rel_omission_by_translation(context):
     context['vis_data'] = json.dumps(context['vis_data'])
 
 
+def visualisation_calendar_view(context):
+
+    query = r'''
+            select
+                le.string as lemma, la.name as language, sc.qt as freq,
+                coalesce(zc.qt::float, 0.0) omitted,
+                coalesce(zc.qt::float, 0.0) / (sc.qt::float) as ratio_omitted
+            from
+                (
+                    select lemma_id, language_id, count(*) qt
+                    from dral_text_occurence oc
+                    group by lemma_id, language_id
+                ) as sc
+                left join
+                (
+                    select lemma_id, language_id, count(*) qt
+                    from dral_text_occurence oc
+                    where zero is true
+                    group by lemma_id, language_id
+                ) as zc
+                on (
+                    sc.language_id = zc.language_id
+                    and sc.lemma_id = zc.lemma_id
+                ),
+                dral_text_language la,
+                dral_text_lemma le
+            where sc.language_id = la.id
+            and sc.lemma_id = le.id
+            and la.name = %s
+            order by sc.qt desc
+            ;
+            '''
+
+    data = [
+        (lg, get_rows_from_query(query, [lg])[0:])
+        for lg
+        in ['LT', 'RU', 'POL']
+    ]
+    context['vis_data'] = OrderedDict(data)
+
+    import json
+    context['vis_data'] = json.dumps(context['vis_data'])
+
+
 def get_rows_from_query(query, params):
     from django.db import connection
     with connection.cursor() as cursor:
