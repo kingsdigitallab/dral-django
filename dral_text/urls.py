55747bc446e1d1376b966f17d1920c3e4055481e
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2019-07-08 12:14:28 +0000

    Staff users can now see a link to the data management page in the footer.

--- dral_text/urls.py
+++ dral_text/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('import/', views.view_import),
+    path('import/', views.view_import, name='data_management'),
     path('import/strings/', views.view_upload_occurrences),
     path('import/sentences/', views.view_upload_sentences),
     path('import/texts/', views.view_upload_texts),
